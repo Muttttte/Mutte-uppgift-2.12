aac0e91ec7dfca16d0d09e00e6625630727ef6b5
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:25:59 +0200

    Add project files.

---
+++ Mutte uppgift 2.12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//Mustafauppgift 2.12
+class program
+{
+
+    public static void Main()
+    {
+        int Startavgift = 300;
+        Console.WriteLine("Hur många dagar ska du köra?");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Hur många kilometer ska du köra bilen?");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Startavgift + x * 500 - 500 + y);
+
+    }
+
+}
+
+
+
+
+
